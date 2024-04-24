6bf15a3f4c0b629535107e0388d6f49ab12504bd
nayzawaungnza <nayzawaung750@gmail.com>
2024-04-24 09:18:25 +0630

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 function App() {
   return (
    <div>
-    <h1>Blogs</h1>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul className="navbar-nav ">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+            
+        </div>
+    </div>
+</nav>
    </div>
   );
 }
